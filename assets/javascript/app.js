b7d1946c0866548679d41ecaa6d351059d738e00
jenenneihart <neihart.jenen@gmail.com>
2020-07-21 19:50:39 -0700

    able to finish game, questions are good, added styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
-var card = $("quiz-area");
+var card = $("#quiz-area");
 
 var testQuestions = [
     {
-      question: "Our love is like a red, red rose... and I am a little thorny.",
+      question: " Our love is like a red, red rose... and I am a little thorny. ",
       answers: [
          "Barb Wire",
          "The Mask",
@@ -48,7 +48,7 @@ var testQuestions = [
   var game = {
     correct: 0,
     wrong: 0,
-    counter: 180,
+    counter: 130,
 
     countdown: function() {
       game.counter--;
@@ -63,7 +63,7 @@ var testQuestions = [
        timer = setInterval(game.countdown, 1000);
 
        $("#secondary-wrapper").prepend(
-         "<h2>Time Remaining: <span id='counter-number'>180</span> Seconds</h2>"
+         "<h2>Time Remaining: <span id='counter-number'>130</span> Seconds</h2>"
        );
        
    $("#start").remove();
@@ -72,18 +72,18 @@ var testQuestions = [
     card.append("<h2>" + testQuestions[i].question + "</h2>");
     for (var j = 0; j < testQuestions[i].answers.length; j++) {
       card.append("<input type='radio' name='question-" + i +
-        "'value='" + testQuestions[i].answers[j] + "''>" + testQuestions[i].answers[j]);
+        "' value='" + testQuestions[i].answers[j] + "''>" + testQuestions[i].answers[j]);
     }
   }
 
   card.append("<button id='submit'>Submit</button>");
-    },
+  },
 
     
   done: function() {
   var inputs = card.children("input:checked");
     for (var i = 0; i < inputs.length; i++) {
-      if ($(inputs[i]).val() === myQuestions[i].correctAnswer) {
+      if ($(inputs[i]).val() === testQuestions[i].correctAnswer) {
         game.correct++;
         } else {
           game.incorrect++;
@@ -92,16 +92,15 @@ var testQuestions = [
       this.result();
     },
   
-    result: function() {
-      clearInterval(timer);
+  result: function() {
+    clearInterval(timer);
   
-      $("#secondary-wrapper h2").remove();
+    $("#secondary-wrapper h2").remove();
   
-      card.html("<h2>All Done!</h2>");
+      card.html("<h2>You've Finished!</h2>");
       card.append("<h3>Correct Answers: " + this.correct + "</h3>");
       card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
     }
-  
   };
  
 
@@ -109,7 +108,7 @@ $(document).on("click", "#start", function() {
   game.start();
 });
   
-$(document).on("click", "#done", function() {
+$(document).on("click", "#submit", function() {
   game.done();
 });
   
